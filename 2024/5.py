adbe15bb2a614fd8aceebe74f84d3de1ad734a4a
Kim Faint <kim.faint@gmail.com>
2024-12-11 00:29:32 +1000

    Day 5 Part 2

--- 2024/5.py
+++ 2024/5.py
@@ -2,6 +2,7 @@
 
 import sys
 from pathlib import Path
+import functools
 from pprint import pprint
 
 filename = sys.argv[0].split(".")[0]
@@ -29,10 +30,10 @@ with open(input_file, 'r') as f:
 pprint(rules)
 pprint(updates)
 
-
 # Iterate over each update and assess each agains the full list of rules
 # adding to correct_updates if by the end no rules are broken
 correct_updates = []
+incorrect_updates = []
 for update in updates:
     valid = True
     for a, b in rules:
@@ -42,10 +43,42 @@ for update in updates:
                 break
     if valid:
         correct_updates.append(update)
-pprint(correct_updates)
+    else:
+        incorrect_updates.append(update)
 
 # Add middle numbers in each correct_update
-sum = 0
+pprint(correct_updates)
+correct_sum = 0
 for update in correct_updates:
-    sum += update[int(len(update) / 2)]
-print(f"{sum = }")
+    correct_sum += update[int(len(update) / 2)]
+
+
+def cmp_rules(l, r):
+    """
+    If l,r are not in any rules that they are even, return 0
+    If l,r are in a rule, if the order is correct, return -1 to indacate order correct, 
+    else return 1 to indicate out-of-order
+    """
+    broken = 0
+    for rule in rules:
+        if l in rule and r in rule:
+            broken = -1
+            if l != rule[0]:
+                broken = 1
+                break
+    return broken
+
+
+# Iterate over incorrect_updates and sort them using the rules
+pprint(incorrect_updates)
+for update in incorrect_updates:
+    update.sort(key=functools.cmp_to_key(cmp_rules))
+
+# Add middle numbers in each correct_update
+pprint(incorrect_updates)
+incorrect_sum = 0
+for update in incorrect_updates:
+    incorrect_sum += update[int(len(update) / 2)]
+
+print(f"{correct_sum = }")
+print(f"{incorrect_sum = }")
